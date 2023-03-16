2f2b7a2463a9a854cc70b7dfaafad98bdd8430ca
mizunt1 <mizunt@gmail.com>
2023-03-16 15:50:16 -0400

    Update init.el

--- init.el
+++ init.el
@@ -125,7 +125,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.                                                                                                                                                                                                                           
  ;; Your init file should contain only one such instance.                                                                                                                                                                                                                                  
  ;; If there is more than one, they won't work right.                                                                                                                                                                                                                                      
-;; Configure flymake for Python                                                                                                                                                                                                                                                            
+;; Configure flymake for Python 
+;;https://pylint.pycqa.org/en/v2.11.1/user_guide/ide-integration.html
 (when (load "flymake" t)
   (defun flymake-pylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
